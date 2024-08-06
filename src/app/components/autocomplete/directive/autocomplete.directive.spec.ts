1459a4f4aa0329e46380a88ea710a169ad2edc05
CodeSandbox <support@codesandbox.io>
2024-08-06 16:07:18 +0000

    Sandbox snapshot commit

--- src/app/components/autocomplete/directive/autocomplete.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { C3AutocompleteDirective } from './autocomplete.directive';
-
-describe('AutocompleteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new C3AutocompleteDirective();
-    expect(directive).toBeTruthy();
-  });
-});
