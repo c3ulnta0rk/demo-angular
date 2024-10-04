97a5be5a0ac3656589fd4f276b654a8d91b80afe
CodeSandbox <support@codesandbox.io>
2024-10-04 19:15:03 +0000

    Sandbox snapshot commit

--- src/app/directives/underline.directive.ts
+++
@@ -1,7 +0,0 @@
-import { Directive } from '@angular/core';
-
-@Directive({
-  selector: '[c3Underline]',
-  standalone: true,
-})
-export class UnderlineDirective { }
--- src/app/pages/button-underline/button-underline.component.ts
+++ src/app/pages/button-underline/button-underline.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { UnderlineDirective } from '../../directives/underline.directive';
 
 @Component({
   selector: 'c3-button-underline',
   standalone: true,
-  imports: [CommonModule, UnderlineDirective],
+  imports: [CommonModule],
   templateUrl: './button-underline.component.html',
   styleUrl: './button-underline.component.scss',
 })
-export class ButtonUnderlineComponent {}
+export class ButtonUnderlineComponent {
+}
